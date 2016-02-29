4aafca59e329af4d6e8b579da939cf71d8d9a7e7
Svetlana-Kovalev <svetlana.kovalev@gmail.com>
2016-03-01 00:21:14 +0200

    updated tests in GameTests

--- Problem3/FourInLineTests/GameTests.cs
+++ Problem3/FourInLineTests/GameTests.cs
@@ -2,6 +2,7 @@ using FourInLineConsole.DataTypes;
 using FourInLineConsole.Interfaces;
 using FourInLineConsole.Interfaces.Board;
 using FourInLineConsole.Interfaces.Player;
+using Moq;
 using NUnit.Framework;
 
 namespace FourInLineTests
@@ -18,8 +19,21 @@ namespace FourInLineTests
             IGame game = new Game(board, player1, player2);
 
             Assert.That(game.Board, Is.SameAs(board));
+            Assert.That(game.Board.Status, Is.EqualTo(BoardStatus.Active));
             Assert.That(game.Player1, Is.SameAs(player1));
             Assert.That(game.Player2, Is.SameAs(player2));
+        }
+
+        [Test]
+        public void BoardStatusTakeValueFromBoard()
+        {
+            var mock = new Mock<IBoard>();
+            IPlayer player1 = new HumanPlayer("1");
+            IPlayer player2 = new HumanPlayer("2");
+            IGame game = new Game(mock.Object, player1, player2);
+
+            Assert.That(game.Status, Is.Not.Null);
+            mock.VerifyGet(foo => foo.Status);
         }
     }
 }
